129f0f141c224d52285735ecf0684817d0d13ae8
Michael Chen <3814531+Sticksword@users.noreply.github.com>
2019-11-20 11:56:43 -0800

    fix line chart for demo

--- app/components/performance-dashboard.js
+++ app/components/performance-dashboard.js
@@ -49,34 +49,27 @@ function getDates(startDate, stopDate) {
   var currentDate = moment(startDate);
   var endDate = moment(stopDate);
   while (currentDate <= endDate) {
-    dateArray.push( moment(currentDate).format('YYYY-MM-DD') )
+    dateArray.push( moment(currentDate).format('MM-DD-YYYY') )
     currentDate = moment(currentDate).add(1, 'days');
   }
   return dateArray;
 }
-
 const LINECHARTDATA = {
   labels: getDates('11-01-2019', '11-06-2019'),
   datasets: [{
-    label: '# of Votes',
+    label: 'dataset 1',
     data: [12, 19, 3, 5, 2, 3],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-      'rgba(255, 99, 132, 1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)'
-    ],
-    borderWidth: 1
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 0.2)',
+    fill: false,
+    yAxisID: 'y-axis'
+  }, {
+    label: 'dataset 2',
+    data: [1, 2, 3, 4, 5, 6],
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgba(54, 162, 235, 0.2)',
+    fill: false,
+    yAxisID: 'y-axis'
   }]
 };
 
